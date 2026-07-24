a6a34c6ef485145aac491cbed4153c4afe221941
MnemonifyOrg <sebastinkf@yahoo.com>
2026-07-23 21:33:34 -0500

    Fix glossary test React import

--- packages/player/src/components/GlossaryPayload.test.jsx
+++ packages/player/src/components/GlossaryPayload.test.jsx
@@ -1,4 +1,5 @@
 import { describe, expect, it } from 'vitest';
+import React from 'react';
 import { renderToStaticMarkup } from 'react-dom/server';
 import GlossaryPayload from './GlossaryPayload.jsx';
 import Modal from './Modal.jsx';
